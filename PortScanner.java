3e572b3d3bfe745fab3bd44488fa4c2e5f5ea4e0
juniarakhsan <124061925+juniarakhsan@users.noreply.github.com>
2023-10-30 13:29:18 +0700

    Add files via upload

---
+++ PortScanner.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class PortScanner {
+    public static void main(String[] args) throws Exception {
+        String host = "localhost";
+        InetAddress inetAddress = InetAddress.getByName(host);
+        String hostName = inetAddress.getHostName();
+
+        for (int port = 1; port <= 80; port++) {
+            try {
+                Socket socket = new Socket(hostName, port);
+                String text = hostName + " is listening on port " + port;
+                System.out.println(text);
+                socket.close();
+            } catch (IOException e) {
+                String s = hostName + " is not listening on port " + port;
+                System.out.println(s);
+            }
+        }
+    }
+}
